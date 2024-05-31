22734266ca6635886df9305372479416b26492d8
Elsie <elsie19@pm.me>
2024-05-30 20:26:10 -0500

    Yep

---
+++ README.md
@@ -0,0 +1,3 @@
+## SRCINFO_BASH
+
+`SRCINFO_BASH` is a one file sourced library for bash that parses [SRCINFO](https://wiki.archlinux.org/title/.SRCINFO) files into native bash dictionaries.
--- src/srcinfo.sh
+++ src/srcinfo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 function srcinfo.parse_key_val() {
-    local key value input="${1}"
-    declare -n out_array="${2}"
-    out_array=(${input/=/ })
+    local key value split=() input="${1}"
+    declare -nA out_array="${2}"
+    split=(${input/=/ })
+    out_array=([key]="${split[0]}" [value]="${split[1]}")
 }
